2ad94d62b305f470a65e89a045da479ce9fd213f
Aleksandar Bursać <aleksandarbursac150699@gmail.com>
2024-08-07 10:18:49 +0200

    Initial commit ALLAPP

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mysql = require('mysql2');
+const app = express();
+const port = 3001;
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'mynewshopapp'
+});
+
+app.get('/', (req, res) => {
+    res.send("Hello World");
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+    
+});
