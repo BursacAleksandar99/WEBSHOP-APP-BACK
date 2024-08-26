54f9a797bf6110e8e5b4d5dc815b80ed4c60cc97
Aleksandar Bursać <aleksandarbursac150699@gmail.com>
2024-08-26 20:29:09 +0200

    - Implemented PUT route to update processor details. - Implemented DELETE route to remove a processor from the database. - Tested both routes using Insomnia to ensure proper functionality.

--- server/routes/Processors.js
+++ server/routes/Processors.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { Processors } = require('../models');
 const multer = require('multer');
 const path = require('path');
+const { error } = require('console');
 
 
 const storage = multer.diskStorage({
@@ -37,6 +38,40 @@ router.get('/', async(req, res) => {
         res.json(processors);
     } catch(error){
         res.status(500).json({ error: "Faild to fetch processors!"});
+    }
+})
+
+router.put('/:id', upload.single('imageUrl'), async (req, res) => {
+    const { id } = req.params;
+    const { name, model, cores, threads, baseClock, boostClock, price } = req.body;
+    const imageUrl = req.file ? req.file.path.replace(/\\/g, '/') : null;
+
+    try{
+        const processor = await Processors.findByPk(id);
+        if(!processor){
+            return res.status(404).json({error: "Processor not found!"});
+        }
+        await processor.update({
+            name, model, cores, threads, baseClock, boostClock, price, imageUrl
+        });
+        res.json(processor);
+    } catch(error){
+        res.status(500).json({error: "Faild to update processor!"});
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    const { id } = req.params;
+
+    try{
+        const processor = await Processors.findByPk(id);
+        if(!processor){
+            return res.status(404).json({error: "Processor not found!"});
+        }
+        await processor.destroy();
+        res.status(204).send();
+    } catch(error){
+        res.status(500).json({error: "Faild to delete processor!"});
     }
 })
 
